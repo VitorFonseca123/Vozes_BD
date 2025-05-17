fe45882abdbf3b757fcbcf99fb32d9299078c97d
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2025-05-17 12:21:01 -0300

    arruma comentario

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def iniciaDB():
-    client = chromadb.PersistentClient(path="./chromadb")
-'''
+
+    '''
     precisa configurar a collection
     https://docs.trychroma.com/docs/collections/configure
-'''
+    '''
+    client = chromadb.PersistentClient(path="./chromadb")
     try:
         collection = client.create_collection(name="teste")
     except chromadb.errors.InternalError as e:
